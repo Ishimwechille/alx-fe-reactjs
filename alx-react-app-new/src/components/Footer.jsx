71acd9937fd9d09a6f17dfc024a1ceb9fe35bb68
Ishimwechille <012220538@isbatuniversity.com>
2025-11-08 17:08:22 +0300

    new work

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer style={{ 
+      backgroundColor: 'navy', 
+      color: 'white', 
+      textAlign: 'center', 
+      padding: '15px',
+      marginTop: '20px'
+    }}>
+      <p>© 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
