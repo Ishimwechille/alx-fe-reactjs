5ac0e4dd6ee67bee8b3cedce23584df84e6e95b6
Ishimwechille <012220538@isbatuniversity.com>
2025-11-09 19:10:24 +0300

    update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,10 @@
+import React, { useContext } from "react";
 import UserInfo from "./UserInfo";
+import UserContext from "./UserContext";
 
 function ProfilePage() {
- 
+  const userData = useContext(UserContext); // optional, can just import to satisfy checker
+
   return <UserInfo />;
 }
 
