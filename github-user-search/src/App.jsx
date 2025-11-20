362e61791847fcabb3151327d185d52e5c5f8756
Ishimwechille <012220538@isbatuniversity.com>
2025-11-20 18:57:51 +0300

    Remove .env from version control

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import './App.css';
+import Search from './components/Search';
 
 function App() {
+
   return (
-    <div>
-      <h1>GitHub User Search</h1>
-      {/* Future components will go here */}
-    </div>
+    <>
+      <div className="min-h-screen  text-[#f8f9fa] flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold mb-8 text-[#6c757d]">
+          Search GitHub Like a Pro!
+        </h1>
+        <Search />
+      </div>
+    </>
   );
 }
 
