ee6bfb6c0b4b5665a133f4bf511729177a653e60
Ishimwechille <012220538@isbatuniversity.com>
2025-12-10 10:55:42 +0300

    new update

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Profile() {
+  return (
+    <div>
+      <h2>User Profile</h2>
+
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+
+      {/* Outlet renders nested routes */}
+      <Outlet />
+    </div>
+  );
+}
